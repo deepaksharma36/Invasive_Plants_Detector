a3f465d04a1797fe13c95e25b0501902ad152c40
Deepak Sharma <deepak.jiitn@gmail.com>
2018-04-11 12:02:29 -0400

    separating execution code from sw_classifier

---
+++ python/src/experiments/execute_sw_classifier.py
@@ -0,0 +1,59 @@
+import sys
+from src.utils.cfg import yfile_to_cfg
+from src.tools.sw_classifier import SW_Classifier
+
+def jap_vs_all(cfg, sw_classifier):
+    jap_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all_hn'
+    #jap_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all'
+    jap_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all_complete'
+    jap_data_dir_undivided = './dataset/data_train_test_splited/one_vs_all/jap_vs_all'
+    colors = ['aqua', 'darkorange', 'cornflowerblue']
+    sw_classifier.assign_datasets(jap_train_data_dir,
+                                  jap_data_dir, jap_data_dir_undivided)
+    #sw_classifier.pick_scores = False
+    #sw_classifier.pick_scores = True
+    jap_best_model_path =\
+            '/home/deepak/invasive_detector/dataset/data_train_test_splited/one_vs_all_sub_images_take2//jap_vs_all_hn/trained_models/densenet121_model_best.pth.tar'
+    #        '/home/deepak/invasive_detector/dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all/trained_models/dense_model_best.pth.tar'
+    sw_classifier.window_avg = 6
+    #jap_best_model_path = sw_classifier.one_vs_all_train(colors, jap_best_model_path)
+    #jap_best_model_path = sw_classifier.one_vs_all_train(colors)
+    sw_classifier.one_vs_all_sw(jap_best_model_path, colors)
+
+def phrag_vs_all(cfg, sw_classifier):
+    phrag_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/phrag_vs_all_hn'
+    #phrag_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/phrag_vs_all'
+    phrag_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/phrag_vs_all_complete'
+    phrag_data_dir_undivided = './dataset/data_train_test_splited/one_vs_all/phrag_vs_all'
+    sw_classifier.assign_datasets(phrag_train_data_dir,
+                                  phrag_data_dir, phrag_data_dir_undivided)
+    phrag_best_model_path =\
+            '/home/deepak/invasive_detector/dataset/data_train_test_splited/one_vs_all_sub_images_take2//phrag_vs_all_hn/trained_models/densenet121_model_best.pth.tar'
+    colors = ['red', 'green', 'cornflowerblue']
+    sw_classifier.window_avg = 8
+    #pharg_best_model_path = sw_classifier.one_vs_all_train(colors, phrag_best_model_path)
+    #phrag_best_model_path = sw_classifier.one_vs_all_train(colors)
+    sw_classifier.one_vs_all_sw(phrag_best_model_path, colors)
+
+def implementation_test(cfg, sw_classifier):
+    data_dir = './hymenoptera_data'
+    colors = ['red', 'green', 'cornflowerblue']
+    sw_classifier.assign_datasets(data_dir, data_dir, data_dir)
+    test_best_model_path = sw_classifier.one_vs_all_train(colors)
+    sw_classifier.one_vs_all_sw(test_best_model_path, colors)
+
+
+
+def main():
+    assert len(sys.argv) > 1, "cfg file path missing"
+    cfg = yfile_to_cfg(sys.argv[1])
+    sw_classifier = SW_Classifier(cfg)
+    #  implementation_test(cfg, sw_classifier)
+    phrag_vs_all(cfg, sw_classifier)
+    jap_vs_all(cfg, sw_classifier)
+    input()
+    input()
+
+
+if __name__ == '__main__':
+    main()
--- python/src/tools/sw_classifier.py
+++ python/src/tools/sw_classifier.py
@@ -1,10 +1,8 @@
-import sys
 import matplotlib.pyplot as plt
 from src.tools.finetune_densnet import TrainDensnet
 #from src.tools.data_split import DataSplit
 from torchnet.meter import ConfusionMeter
 from src.evaluation.plot_roc_pr import Evaluation
-from src.utils.cfg import yfile_to_cfg
 from src.utils.geometric import check_overlapping
 from src.tools.data_split import DataSplit
 
@@ -308,65 +306,3 @@ class SW_Classifier():
                                 colors, 'Crop Test Set Subset')
         evaluation.plot_results()
         return best_model_path
-
-
-def jap_vs_all(cfg, sw_classifier):
-    jap_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all_hn'
-    #jap_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all'
-    jap_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all_complete'
-    jap_data_dir_undivided = './dataset/data_train_test_splited/one_vs_all/jap_vs_all'
-    colors = ['aqua', 'darkorange', 'cornflowerblue']
-    sw_classifier.assign_datasets(jap_train_data_dir,
-                                  jap_data_dir, jap_data_dir_undivided)
-    #sw_classifier.pick_scores = False
-    #sw_classifier.pick_scores = True
-    #jap_best_model_path =\
-    #        '/home/deepak/invasive_detector/dataset/data_train_test_splited/one_vs_all_sub_images_take2/jap_vs_all/trained_models/dense_model_best.pth.tar'
-    #jap_best_model_path = sw_classifier.one_vs_all_train(colors, jap_best_model_path)
-    jap_best_model_path = sw_classifier.one_vs_all_train(colors)
-    sw_classifier.one_vs_all_sw(jap_best_model_path, colors)
-
-def phrag_vs_all(cfg, sw_classifier):
-    phrag_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/phrag_vs_all_hn'
-    #phrag_train_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/phrag_vs_all'
-    phrag_data_dir = './dataset/data_train_test_splited/one_vs_all_sub_images_take2/phrag_vs_all_complete'
-    phrag_data_dir_undivided = './dataset/data_train_test_splited/one_vs_all/phrag_vs_all'
-    sw_classifier.assign_datasets(phrag_train_data_dir,
-                                  phrag_data_dir, phrag_data_dir_undivided)
-    #phrag_best_model_path =\
-    #        '/home/deepak/invasive_detector/dataset/data_train_test_splited/one_vs_all_sub_images_take2//phrag_vs_all/trained_models/densenet121_model_best.pth.tar'
-    colors = ['red', 'green', 'cornflowerblue']
-    #pharg_best_model_path = sw_classifier.one_vs_all_train(colors, phrag_best_model_path)
-    phrag_best_model_path = sw_classifier.one_vs_all_train(colors)
-    sw_classifier.one_vs_all_sw(phrag_best_model_path, colors)
-
-def implementation_test(cfg, sw_classifier):
-    data_dir = './hymenoptera_data'
-    colors = ['red', 'green', 'cornflowerblue']
-    sw_classifier.assign_datasets(data_dir, data_dir, data_dir)
-    test_best_model_path = sw_classifier.one_vs_all_train(colors)
-    sw_classifier.one_vs_all_sw(test_best_model_path, colors)
-
-    '''
-    jap_train_data_dir = data_dir
-    phrag_train_data_dir = data_dir
-    jap_data_dir = data_dir
-    phrag_data_dir = data_dir
-    jap_data_dir_undivided = data_dir
-    phrag_data_dir_undivided= data_dir
-    #data_dir_undivided = './hymenoptera_data
-    '''
-
-
-def main():
-    assert len(sys.argv) > 1, "cfg file path missing"
-    cfg = yfile_to_cfg(sys.argv[1])
-    sw_classifier = SW_Classifier(cfg)
-    #implementation_test(cfg, sw_classifier)
-    phrag_vs_all(cfg, sw_classifier)
-    jap_vs_all(cfg, sw_classifier)
-    input()
-    input()
-
-if __name__ == '__main__':
-    main()
